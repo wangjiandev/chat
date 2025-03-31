fa5ec1c7eeb800cf127d1df89e1548e1dd194cd8
Wang Jian <wangjian0504@gmail.com>
2025-03-31 19:21:03 +0800

    chore: refactor code to workspace and add notify_server project

--- src/handlers/message.rs
+++
@@ -1,9 +0,0 @@
-use axum::response::IntoResponse;
-
-pub(crate) async fn message_list_handler() -> impl IntoResponse {
-    "message_list"
-}
-
-pub(crate) async fn message_create_handler() -> impl IntoResponse {
-    "message_create"
-}
